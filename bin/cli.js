3cdb4def7f8dc7dd9209a45a2996ffb901ec765d
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-06-07 20:43:34 +0100

    cli

--- bin/cli.js
+++ bin/cli.js
@@ -1,4 +1,13 @@
 #!/usr/bin/env node
 "use strict";
 
-var boil = require("boil-js");
+var boil = require("boil-js"),
+    config = require("../boil.json");
+
+var args = process.argv.slice(2);
+if (args.length){
+    config = boil.setArgs(config, args);
+    boil.renderRecipe(config, "package");
+} else {
+    console.error("usage: package <name>");
+}
